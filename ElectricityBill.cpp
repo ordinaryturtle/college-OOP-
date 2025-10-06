35a0667bf99212fa0fac209811e466665a8f53d2
ordinaryturtle <ordinaryturtle01@gmail.com>
2025-10-06 21:03:51 +0530

    Add files via upload

---
+++ ElectricityBill.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double units,bill;
+    cout<<"Enter units: "; 
+    cin>>units;
+    if(units<=100) 
+        bill=units*1.5;
+    else if(units<=200) 
+        bill=100*1.5+(units-100)*2.5;
+    else bill=100*1.5+100*2.5+(units-200)*4.0;
+    if(bill>500) 
+        bill+=bill*0.1;
+    cout<<"Total Bill: Rs "<<bill;
+    return 0;
+}
